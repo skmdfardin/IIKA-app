e52bee26ff75252abdca90c06379643a95cdd775
Ganesh Savitru <88309309+GaneshSavitruDC@users.noreply.github.com>
2022-07-11 14:57:08 +0530

    New user landing page in progress

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -2,8 +2,9 @@ import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 //import SplashScreen from '../screens/static/SplashScreen';
-import SignUp from '../screens/user/SignUp';
+//import SignUp from '../screens/user/SignUp';
 // import SignIn from '../screens/user/SignIn';
+import NewUserLanding from '../screens/user/NewUserLanding';
 
 const StackNavigation: FC = () => {
   const Stack = createNativeStackNavigator();
@@ -12,7 +13,8 @@ const StackNavigation: FC = () => {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         {/* <Stack.Screen name="splash_screen" component={SplashScreen} /> */}
         {/* <Stack.Screen name="sign_in" component={SignIn} /> */}
-        <Stack.Screen name="sign_up" component={SignUp} />
+        {/* <Stack.Screen name="sign_up" component={SignUp} /> */}
+        <Stack.Screen name="new_user_landing" component={NewUserLanding} />
       </Stack.Navigator>
     </NavigationContainer>
   );

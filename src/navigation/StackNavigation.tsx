4efbce2a2fdfabe195086da09d9380a44b5d7014
Ganesh Savitru <88309309+GaneshSavitruDC@users.noreply.github.com>
2022-07-27 18:10:58 +0530

    add farm dummy

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -7,18 +7,20 @@ import SignIn from '../screens/user/SignIn';
 import SignUp from '../screens/user/SignUp';
 import SplashScreen from '../screens/static/SplashScreen';
 import EditProfileScreen from '../screens/user/EditProfile';
+import AddFarm from '../screens/farm/AddFarm';
 
 const StackNavigation: FC = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name={SPLASH_SCREEN} component={SplashScreen} />
+        {/* <Stack.Screen name={SPLASH_SCREEN} component={SplashScreen} />
         <Stack.Screen name={SLIDER_SCREEN} component={SliderScreen} />
         <Stack.Screen name={SIGN_IN} component={SignIn} />
         <Stack.Screen name={SIGN_UP} component={SignUp} />
         <Stack.Screen name={NEW_USER_LANDING} component={NewUserLanding} />
-        <Stack.Screen name={EDIT_PROFILE_SCREEN} component={EditProfileScreen} />
+        <Stack.Screen name={EDIT_PROFILE_SCREEN} component={EditProfileScreen} /> */}
+        <Stack.Screen name={ADD_FARM} component={AddFarm} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -29,5 +31,6 @@ export const SIGN_IN: string = 'sign_in';
 export const SIGN_UP: string = 'sign_up';
 export const NEW_USER_LANDING: string = 'new_user_landing';
 export const EDIT_PROFILE_SCREEN = 'edit_profile_screen';
+export const ADD_FARM = 'add_farm';
 
 export default StackNavigation;
